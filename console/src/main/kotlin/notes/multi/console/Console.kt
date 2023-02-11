013c82e97b777a0d427e65908f9ad545347b52c7
Abhay Menon <a32menon@uwaterloo.ca>
2023-02-11 01:42:11 -0500

    Added pseudocode for handling user arguments

--- console/src/main/kotlin/notes/multi/console/Console.kt
+++ console/src/main/kotlin/notes/multi/console/Console.kt
@@ -3,10 +3,63 @@
  */
 package notes.multi.console
 
-import notes.multi.utilities.StringUtils
+import notes.multi.app.MessageUtils
+import notes.multi.utilities.Filemanager
+import java.lang.IllegalArgumentException
 
-import org.apache.commons.text.WordUtils
+fun main(args: Array<String>) {
 
-fun main() {
-    println("this is multi setup")
+    if(args.isEmpty()) {
+        println("""
+        +-------------------------------------------------------------------------------------+
+        WELCOME TO TEAM 112'S CONSOLE BASED NOTES APP
+         
+        This application is a working console-based prototype for the
+        note taking application that the team will be delivering at the end of the term.
+        
+        With this simple app, users can:
+            1. Create Notes
+            2. Edit Notes
+            3. Delete Notes
+            
+         1. Create Notes
+            - To create notes, users must type "notes <filename>"
+            
+         2. Edit Notes
+            - To edit notes, users must type "notes <filename>"
+            - If the file doesn't exist, a new note will be created
+            
+         3. Delete Notes
+            - To delete a note, users must type "notes -d <filename>"
+            - If the user tries to delete a note that doesn't exist,
+              the app will display an error message
+        +-------------------------------------------------------------------------------------+
+    """.trimIndent())
+    } else {
+        if (args.size < 2) {
+            val filename = args[0]
+           // Optional Regex Check for a specific argument format:
+              MessageUtils.verifyFilename(filename, Regex("^.*[.](md|txt)$"))
+           // if (fileExists) {
+           //     Filemanager(".").editfile()
+           // } else {
+           //     Filemanager(".").createfile()
+           // }
+        } else {
+            if (args[0] != "-d") {
+                throw IllegalArgumentException("[ERROR]: First parameter must be the delete flag")
+            } else {
+                val filename = args[1]
+
+                // Optional Regex Check for a specific argument format:
+                MessageUtils.verifyFilename(filename, Regex("^.*[.](md|txt)$"))
+                // if (fileExists) {
+                //     Filemanager(".").deletefile()
+                // } else {
+                //     throw IllegalArgumentException("[ERROR]: File must exist for it to be deleted")
+                // }
+            }
+        }
+    }
+
 }
