f0f9452d4571d59a9977dc0a0b97617f233f815f
thaihoangdang <thaihoangdang1803@gmail.com>
2023-03-09 21:51:23 -0500

    modularizing DatabaseOperations

--- console/src/main/kotlin/notes/multi/console/Console.kt
+++ console/src/main/kotlin/notes/multi/console/Console.kt
@@ -9,6 +9,7 @@ import java.lang.IllegalArgumentException
 
 import notes.multi.utilities.TextWindow
 import javafx.application.Application
+import notes.multi.utilities.DatabaseOperations
 import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -22,27 +23,27 @@ import java.time.LocalDate
 import java.time.LocalDateTime
 import kotlin.io.path.createTempDirectory
 
-var conn: Connection? = null
-fun connect() {
-    try {
-        val url = "jdbc:sqlite:chinook.db"
-        conn = DriverManager.getConnection(url)
-        println("Connection established.")
-    } catch (e: SQLException) {
-        println(e.message)
-    }
-}
-
-fun query() {
-    try {
-        if (conn != null) {
-            // do something
-            println("Database successfully connected!")
-        }
-    } catch (err: Exception) {
-        System.err.println("Something's wrong!")
-    }
-}
+//var conn: Connection? = null
+//fun connect() {
+//    try {
+//        val url = "jdbc:sqlite:chinook.db"
+//        conn = DriverManager.getConnection(url)
+//        println("Connection established.")
+//    } catch (e: SQLException) {
+//        println(e.message)
+//    }
+//}
+//
+//fun query() {
+//    try {
+//        if (conn != null) {
+//            // do something
+//            println("Database successfully connected!")
+//        }
+//    } catch (err: Exception) {
+//        System.err.println("Something's wrong!")
+//    }
+//}
 
 
 // fun main(args: Array<String>) {
@@ -118,19 +119,6 @@ fun query() {
 //     query()
 // }
 
-object Notes : IntIdTable() {
-    val title: Column<String?> = varchar("title", 100).nullable()
-    val text = text("text", eagerLoading = true).nullable()
-    val dateCreated = varchar("dateCreated", 30)
-    val lastModified = varchar("lastModified", 30)
-}
-
-//object Folder : IntIdTable() {
-//    val folderName = varchar("folderName", 100).nullable()
-//    val notes = arrayOf(Notes.id)
-//    val dateCreated = varchar("dateCreated", 30)
-//    val lastUpdated = varchar("lastUpdated", 30)
-//}
 
 fun main(args: Array<String>) {
     //an example connection to H2 DB
@@ -141,84 +129,26 @@ fun main(args: Array<String>) {
         addLogger(StdOutSqlLogger)
 
         // create a table that reflects the Cities class structure
-        SchemaUtils.create(Notes)
+        SchemaUtils.create(DatabaseOperations.Notes)
 
         // test an instance
         var newNote = Note("Test!", StringBuffer("Hi my name is Hoang"), LocalDate.now().toString(), LocalDateTime.now().toString())
 
         // test addNote
-        addNote(newNote)
+        DatabaseOperations.addNote(newNote)
 
         // reassign instance to a new note with corresponding id
-        newNote = getNote(1)
+        newNote = DatabaseOperations.getNote(1)
         println(newNote.lastModified)
 
         // update note with corresponding id
-        updateNote(3, newNote)
+        DatabaseOperations.updateNote(3, newNote)
 
         // delete note with corresponding id and update id
-        deleteNote(6)
+        DatabaseOperations.deleteNote(6)
 
         // return a MutableArray of notes
-        var listOfNotes = getAllNotes()
+        var listOfNotes = DatabaseOperations.getAllNotes()
         println(listOfNotes.count())
     }
-}
-
-// insert new note
-fun addNote(note: Note) {
-        val newNote = Notes.insert {
-            it[Notes.title] = note.title
-            it[Notes.text] = note.text.toString()
-            it[Notes.dateCreated] = note.dateCreated
-            it[Notes.lastModified] = note.lastModified
-        } get Notes.id
-}
-
-
-fun getNote(id: Int): Note {
-    val tempNote = Note()
-
-    Notes.select { Notes.id eq id }.forEach {
-        tempNote.title = it[Notes.title]
-        tempNote.text = StringBuffer(it[Notes.text])
-        tempNote.dateCreated = it[Notes.dateCreated]
-        tempNote.lastModified = it[Notes.lastModified]
-    }
-
-    return tempNote
-}
-
-fun updateNote(id: Int, note: Note) {
-    Notes.update ({Notes.id eq id}) {
-        it[Notes.title] = note.title
-        it[Notes.text] = note.text.toString()
-        it[Notes.dateCreated] = note.dateCreated
-        it[Notes.lastModified] = note.lastModified
-    }
-}
-
-fun deleteNote(id: Int) {
-    Notes.deleteWhere { Notes.id eq id }
-    updateNoteId()
-}
-
-fun updateNoteId() {
-    var id = 1
-    Notes.slice(Notes.id).selectAll().forEach() {
-        Notes.update ({Notes.id eq it[Notes.id]}) {
-            it[Notes.id] = id
-        }
-        id += 1
-    }
-}
-
-fun getAllNotes(): MutableList<Note> {
-    var listOfNotes: MutableList<Note> = mutableListOf()
-
-    Notes.selectAll().forEach {
-        var tempNote = Note(it[Notes.title], StringBuffer(it[Notes.text]), it[Notes.dateCreated], it[Notes.lastModified])
-        listOfNotes.add(tempNote)
-    }
-    return listOfNotes
 }
