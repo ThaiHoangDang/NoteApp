2022a2d17fb3c3595c4ae06a3e0c2b5bf8ff239e
Guransh Khurana <guranshkhurana@Guranshs-MacBook-Pro.local>
2023-03-24 09:26:11 -0400

    finished with the first iteration of the dark theme.

--- utilities/src/main/kotlin/notes/multi/utilities/Themes.kt
+++ utilities/src/main/kotlin/notes/multi/utilities/Themes.kt
@@ -11,6 +11,7 @@ import javafx.scene.layout.VBox
 import javafx.scene.paint.Color
 import javafx.scene.text.Font
 import javafx.scene.web.HTMLEditor
+import javafx.scene.web.WebView;
 import javax.swing.text.html.StyleSheet
 
 fun toggleDarkMode(scene: Scene, editor: HTMLEditor, isDarkMode: Boolean) {
@@ -20,13 +21,15 @@ fun toggleDarkMode(scene: Scene, editor: HTMLEditor, isDarkMode: Boolean) {
         //note.text.toString(). = Color.WHITE
         //scene.root.style = "-fx-background-color: ${Color.BLACK.toString().replace("0x", "#")}; -fx-text-fill: ${Color.WHITE.toString().replace("0x", "#")}"
         editor.style = "-fx-background-color: white;"
-        editor.htmlText = "<body style='background-color: white;'" + editor.htmlText + "/>"
+        editor.htmlText = "<body style='background-color: white;'" + editor.htmlText
+        scene.stylesheets.remove("darktheme.css")
     } else {
         //note.text = Color.BLACK
         //scene.root.style = "-fx-background-color: ${Color.WHITE.toString().replace("0x", "#")}; -fx-text-fill: ${Color.BLACK.toString().replace("0x", "#")}"
-        editor.htmlText = "<body style='background-color: black;'" + editor.htmlText + "/>"
-        editor.style = "-fx-background-color: black"
+        editor.htmlText = "<body style='background-color: #121212;'" + editor.htmlText
+        editor.style = "-fx-background-color: #121212;"
         scene.stylesheets.add("darktheme.css")
+
 
     }
     //isDarkMode = !isDarkMode
